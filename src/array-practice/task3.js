67bde6c82b6cc33a8668a5a3ae0f79a823c11e62
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-06-10 23:35:27 +0300

    feat: add resolve for task3 from array-practice

--- src/array-practice/task3.js
+++ src/array-practice/task3.js
@@ -9,3 +9,9 @@
  *
  * forEachRight([1, 2, 3, 4], val => console.log(val)); -> в консоль 4 3 2 1
  */
+
+export const forEachRight = (array, cb) =>
+  array
+    .reverse()
+    .slice()
+    .forEach(cb);
