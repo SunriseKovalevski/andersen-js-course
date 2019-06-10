750e3858e6353ac2bfca6d2b009b041080a4bbcd
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-06-11 01:17:42 +0300

    feat: add resolve for task7 from array-practice

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ import forEachRight from './array-practice/task3';
 import union from './array-practice/task4';
 import generator from './array-practice/task5';
 import transformArrayToNumber from './array-practice/task6';
+import take from './array-practice/task7';
 import without from './array-practice/task8';
 import indexOfAll from './array-practice/task9';
 
@@ -139,5 +140,10 @@ console.log(
   'transformArrayToNumber',
   transformArrayToNumber([10, 20, 30], (acc, item) => acc - item)
 );
+
+console.log('=============');
+
+console.log('take', take([1, 2, 3], 5));
+console.log('take', take([1, 2, 3], 2));
 
 console.log('=============');
