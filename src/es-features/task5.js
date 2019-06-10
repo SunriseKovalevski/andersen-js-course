083e29b06311b00f3f2d8741748512c9649ac8c6
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-06-10 18:03:05 +0300

    feat: resolve exercise task5

--- src/es-features/task5.js
+++ src/es-features/task5.js
@@ -16,4 +16,4 @@ export function task5Old(array, obj) {
 // Напишите реализацию функции task5Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export const task5New = (...rest) => rest;
+export const task5New = ([name, val], obj) => [name, val, obj.name, obj.val];
