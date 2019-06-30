0393f64fcf23146b225e1032b7ca291f2c90b1f3
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-06-30 11:59:07 +0300

    docs: prepare task8

---
+++ src/asynchronous-programming/task8.js
@@ -0,0 +1,10 @@
+/*
+const getUsers = url => fetch(url);
+
+function foo(url) {
+  getUsers(url)
+  .then(response => response.json())
+  .then(([ user ]) => console.log(user))
+  .catch((err) => console.log('Error!', err));
+}
+*/
