c93d2654623d833082eff740392f566735cbabf6
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-06-30 14:00:52 +0300

    feat: task 5 ver2

--- src/asynchronous-programming/task5.js
+++ src/asynchronous-programming/task5.js
@@ -1,8 +1,8 @@
-// dfclt
-const first = 'https://www.json-generator.com/api/json/get/cfQCylRjuG';
-const second = 'https://www.json-generator.com/api/json/get/cfDZdmxnDm';
-const third = 'https://www.json-generator.com/api/json/get/cfkrfOjrfS';
-const four = 'https://www.json-generator.com/api/json/get/ceQMMKpidK';
+const first = 'http://www.json-generator.com/api/json/get/cfQCylRjuG';
+const second = 'http://www.json-generator.com/api/json/get/cguaPsRxAi';
+const third = 'http://www.json-generator.com/api/json/get/cfDZdmxnDm';
+const four = 'http://www.json-generator.com/api/json/get/cfkrfOjrfS';
+const five = 'http://www.json-generator.com/api/json/get/ceQMMKpidK';
 
 const fetchJson = async url => {
   const response = await fetch(url);
@@ -15,8 +15,9 @@ const parallel = async () => {
   const secondData = await fetchJson(second);
   const thirdData = await fetchJson(third);
   const fourData = await fetchJson(four);
+  const fiveData = await fetchJson(five);
 
-  console.log(firstData, secondData, thirdData, fourData);
+  console.log(firstData, secondData, thirdData, fourData, fiveData);
 };
 
 parallel();
