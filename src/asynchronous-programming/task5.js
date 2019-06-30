0488e97cf18c5e279e1debd7844840d12c70e802
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-06-30 13:54:18 +0300

    feat: task 5 ver1

--- src/asynchronous-programming/task5.js
+++ src/asynchronous-programming/task5.js
@@ -1,1 +1,22 @@
 // dfclt
+const first = 'https://www.json-generator.com/api/json/get/cfQCylRjuG';
+const second = 'https://www.json-generator.com/api/json/get/cfDZdmxnDm';
+const third = 'https://www.json-generator.com/api/json/get/cfkrfOjrfS';
+const four = 'https://www.json-generator.com/api/json/get/ceQMMKpidK';
+
+const fetchJson = async url => {
+  const response = await fetch(url);
+
+  return response.json();
+};
+
+const parallel = async () => {
+  const firstData = await fetchJson(first);
+  const secondData = await fetchJson(second);
+  const thirdData = await fetchJson(third);
+  const fourData = await fetchJson(four);
+
+  console.log(firstData, secondData, thirdData, fourData);
+};
+
+parallel();
