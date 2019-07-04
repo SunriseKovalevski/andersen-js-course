0e864c4f3c741fbc6a68037f827a78175a1f298d
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-07-04 13:50:40 +0300

    feat: Product model

---
+++ src/server/models/index.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import Product from './product';
+
+const db = 'mongodb://<dbuser>:<dbpassword>@ds345937.mlab.com:45937/andersen';
+
+const connectDb = () => {
+  return mongoose.connect(db, { useNewUrlParser: true, ssl: true, dbName: 'andersen' });
+};
+
+export default {
+  Product,
+  connectDb,
+};
---
+++ src/server/models/product.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    modele: { type: String },
+    link: { type: String },
+    height: { type: Number },
+    weight: { type: Number },
+  },
+  { collection: 'mattresses' }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
